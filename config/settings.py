2c6e9617a7a651be4b0f80d6c5deff50daa271f3
Mirbekov <@kylych-bob>
2023-08-04 15:52:14 +0600

    fixing swagger

--- config/settings.py
+++ config/settings.py
@@ -167,3 +167,13 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('apps.accounts.urls')),
+    path('', include('apps.accounts.urls')),
     # path('api/v2/', include('apps.products.urls')),
-
 ]
 
 
